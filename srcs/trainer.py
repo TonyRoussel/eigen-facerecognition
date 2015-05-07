d391f1dab4378d6211f26a1bf1a4831cb74bb0e4
Tony R <tonydraf@gmail.com>
2015-05-07 16:28:04 +0200

    stuck on gradient descent calculation

--- srcs/trainer.py
+++ srcs/trainer.py
@@ -4,7 +4,7 @@ import training as trn
 from qr import qr
 
 print "toCompute Matrix: \n", mtxLst
-M = trn.concatMatrix(mtxLst) ##########
+M = trn.concatMatrix(mtxLst)
 print "post concatenation Matrix: \n", M
 M = M - M.mean()
 print "post mean sub Matrix: \n", M
@@ -19,3 +19,5 @@ eigenvec = trn.reconstructVector(Mtld, eigenvec)
 print "eigenvec post reconstruct: \n", eigenvec
 eigenvec = np.row_stack((np.ones(np.shape(eigenvec)[1]), eigenvec))
 print "eigenvec post ones: \n", eigenvec
+eigenvec = eigenvec.transpose()
+print "eigenvec post transpose: \n", eigenvec
