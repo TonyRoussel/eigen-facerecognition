6b0e3efae97c7fb12e3b88ebb15bf5e0ff46c6d6
shortyp <tonydraf@gmail.com>
2015-05-18 04:20:06 +0200

    ésplit data acquisition train/valid, add success count

--- srcs/main.py
+++ srcs/main.py
@@ -5,29 +5,29 @@ import files_load as fl
 import sys
 from random import shuffle
 
-if len(sys.argv) == 1:
+if len(sys.argv) != 3:
     exit(1)
-fm = fl.loadmatrixs(sys.argv[1])
-shuffle(fm)
-trainD, validD = fl.split_list(fm, 90)
+trainD = fl.loadmatrixs(sys.argv[1])
+validD = fl.loadmatrixs(sys.argv[2])
+# shuffle(fm)
+# trainD, validD = fl.split_list(fm, 90)
 ftrainD, mtrainD = zip(*trainD)
 
 mean, eigenvec, thetas = train(mtrainD)
 print "Mean: ", mean
 print "eigenvec: \n", eigenvec
 print "thetas: \n", thetas
-# exit(-1)
-# thetaSubmit = submit(mtxLst[0], mean, eigenvec)
-# print "\n"
-# print "M0 delta target", np.absolute(np.sum(thetas[0] - thetaSubmit))
-# print "M1 delta", np.absolute(np.sum(thetas[1] - thetaSubmit))
-# print "M2 delta", np.absolute(np.sum(thetas[2] - thetaSubmit))
-# print "M3 delta", np.absolute(np.sum(thetas[3] - thetaSubmit))
-# print "M4 delta", np.absolute(np.sum(thetas[4] - thetaSubmit))
-# print "M5 delta", np.absolute(np.sum(thetas[5] - thetaSubmit))
-# print "M6 delta", np.absolute(np.sum(thetas[6] - thetaSubmit))
+count = 0
 for idx, data in enumerate(validD):
+    success = False
     filename, mtx = data
     thetaSubmit = subm.submit(mtx, mean, eigenvec)
     matchIdx = subm.compare(thetaSubmit, thetas)
-    print filename, " --> ", ftrainD[matchIdx]
+    if filename[:filename.rfind("_")] == ftrainD[matchIdx][:ftrainD[matchIdx].rfind("_")]:
+        success = True
+        count = count + 1
+    if success is True:
+        print filename, " --> ", ftrainD[matchIdx], "[X]"
+    else:
+        print filename, " --> ", ftrainD[matchIdx], "[ ]"
+print count, " / ", idx + 1, "===>", count / (idx + 1.) * 100, "%"
